0f1c24341b45fe73134f68f53131ba429d2cbb71
Hapsik1234 <dawidosxt@gmail.com>
2023-11-01 19:17:09 +0100

    Server side completed for developing

--- app/src/main/java/Server.java
+++ app/src/main/java/Server.java
@@ -1,3 +1,7 @@
+/* 
+ * Wroten by Dawid Parkitny aka Hapsik1234
+ */
+
 package app.src.main.java;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
@@ -6,53 +10,142 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.*;
 import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.util.HashMap;
 
 public class Server {
     public static void main(String[] args) throws IOException {
 
-        int port = 80; // You can change the port number if needed
+        int port = 80;
         HttpServer server = HttpServer.create(new java.net.InetSocketAddress(port), 0);
         
         // Define a context for handling POST requests
-        server.createContext("/post", new GetHandler());
+        server.createContext("/post", new GetHandler(server));
         
         // Start the server
         server.setExecutor(null); // Use the default executor
         server.start();
         
         System.out.println("Server is listening on port " + port);
+
+        
+
     }
     
     static class GetHandler implements HttpHandler {
+
+        private HttpServer server;
+
+        public GetHandler(HttpServer getserver) {
+            this.server = getserver;
+        }
+
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             if ("GET".equals(exchange.getRequestMethod())) {
 
-                StringBuilder request = new StringBuilder();
-                String queryString = exchange.getRequestURI().getRawQuery();
-                               
-                // Print the POST request data
-                System.out.println("Received GET request:");
-                System.out.println(request.toString());
-                System.out.println(queryString);
+                String paramsRaw = exchange.getRequestURI().getRawQuery();  // Get GET request parameters
                 
-                // You can add code here to process the POST request data as needed
+
+                HashMap<String, String> parameterHashMap = new HashMap<String, String>();   // Define Hashmap of params
                 
-                String response = "GET request received successfully";
+                System.out.println("Recived GET request with params:");
+
+                if (paramsRaw!=null) {
+
+                    String[] params = paramsRaw.toString().split("&");
+
+                    String urlParameter = "url";    // Choose the parameter correcponding for url of the file
+
+                    int urls = 0;   // Numbers of those parameters for checking if there is only one url
+
+                    for (int i=0;i<params.length;i++) {
+
+                        // Fill hashmap with parameters
+                        parameterHashMap.put(params[i].split("=")[0],params[i].split("=")[1]);
+                        System.out.print(params[i].split("=")[0]);
+                        System.out.print(": ");
+                        System.out.println(parameterHashMap.get(params[i].split("=")[0]));
+
+                        if (params[i].split("=")[0].equals(urlParameter)) {
+                            urls++;
+                        }
+                    }
+
+                    if (urls==1) {
+
+                        String file="/asset/secret.txt";
+                        String fileName="secret.txt";
+
+                        // Handling the correct case
+                        System.out.println(parameterHashMap.get(urlParameter));
+
+                        try {
+
+                            new DownloadFile().download(parameterHashMap.get(urlParameter) + file, fileName);
+
+                            System.out.println("Successfully downloaded file!");
+
+                            String response = "<h1>Everything seems good!<h1/>For now from you that's all.";
+                            exchange.sendResponseHeaders(200, response.length());
+                            OutputStream os = exchange.getResponseBody();
+                            os.write(response.getBytes());
+                            os.close();
+
+                            System.out.println("Stopping server.");
+
+                            this.server.stop(5);
+
+                        } catch (IOException e) {
+                            
+                            System.err.print("Exeption error: ");
+                            System.err.println(e);
+
+                        }
+
+
+                    } else {
+                        System.out.println("There are too much or less url parameters! Check syntax and try again.");
+                    }
+                    
+                } else {
+                    System.out.println("No params");
+                }
+
+                
+                
+
+                // Responsing to client
+                String response = "<h1>Something went wrong!<h1/>Please check syntax and try again later.";
                 exchange.sendResponseHeaders(200, response.length());
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
                 os.close();
+
             } else {
-                // Handle other HTTP methods (e.g., GET) here
+
+                System.out.print("Recived ");
+                System.out.print(exchange.getRequestMethod());
+                System.out.println(" request is that an error?");
+
                 String response = "Only GET requests are allowed";
                 exchange.sendResponseHeaders(405, response.length());
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
                 os.close();
             }
+            System.out.print('\n');
+        }
+    }
+
+    static class DownloadFile {
+        public void download(String url, String fileName) throws IOException {
+            InputStream in = new URL(url).openStream();
+            Files.copy(in, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
         }
     }
 }
