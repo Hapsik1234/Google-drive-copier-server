f0b8d8339d6e331119c1b1604689a8877020c77a
Hapsik1234 <dawidosxt@gmail.com>
2023-11-10 19:49:37 +0100

    Drive API almost there

--- app/src/main/java/GoogleDrive.java
+++ app/src/main/java/GoogleDrive.java
@@ -1,182 +1,170 @@
 package app.src.main.java;
 
-import com.google.api.apikeys.v2.ApiKeysClient;
-import com.google.api.apikeys.v2.ApiTarget;
-import com.google.api.apikeys.v2.CreateKeyRequest;
-import com.google.api.apikeys.v2.Key;
-import com.google.api.apikeys.v2.LocationName;
-import com.google.api.apikeys.v2.Restrictions;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.model.File;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
 
+
+/* class to demonstrate use of Drive files list API */
 public class GoogleDrive {
+  /**
+   * Application name.
+   */
+  private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
+  /**
+   * Global instance of the JSON factory.
+   */
+  private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+  /**
+   * Directory to store authorization tokens for this application.
+   */
+  private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
-  public static void run(/*String[] args*/)
-      throws IOException, ExecutionException, InterruptedException, TimeoutException {
-    // TODO(Developer): Before running this sample,
-    //  1. Replace the variable(s) below.
-    //  2. Set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
-    //  3. Make sure you have the necessary permission to create API keys.
-    String projectId = "GOOGLE_CLOUD_PROJECT_ID";
+  /**
+   * Global instance of the scopes required by this quickstart.
+   * If modifying these scopes, delete your previously saved tokens/ folder.
+   */
+  private static final List<String> SCOPES =
+      Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
+  private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
-    createApiKey(projectId);
+  /**
+   * Creates an authorized Credential object.
+   *
+   * @param HTTP_TRANSPORT The network HTTP Transport.
+   * @return An authorized Credential object.
+   * @throws IOException If the credentials.json file cannot be found.
+   */
+  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+      throws IOException {
+    // Load client secrets.
+    InputStream in = GoogleDrive.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+    if (in == null) {
+      throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+    }
+    GoogleClientSecrets clientSecrets =
+        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+    // Build flow and trigger user authorization request.
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+        .setAccessType("offline")
+        .build();
+    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+    Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    //returns an authorized Credential object.
+    return credential;
   }
 
-  // Creates an API key.
-  public static void createApiKey(String projectId)
-      throws IOException, ExecutionException, InterruptedException, TimeoutException {
-    // Initialize client that will be used to send requests. This client only needs to be created
-    // once, and can be reused for multiple requests. After completing all of your requests, call
-    // the `apiKeysClient.close()` method on the client to safely
-    // clean up any remaining background resources.
-    try (ApiKeysClient apiKeysClient = ApiKeysClient.create()) {
+  public static void run(/*String... args*/) throws IOException, GeneralSecurityException {
+    // Build a new authorized API client service.
+    final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+    Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+        .setApplicationName(APPLICATION_NAME)
+        .build();
 
-      Key key = Key.newBuilder()
-          .setDisplayName("My first API key")
-          // Set the API key restriction.
-          // You can also set browser/ server/ android/ ios based restrictions.
-          // For more information on API key restriction, see:
-          // https://cloud.google.com/docs/authentication/api-keys#api_key_restrictions
-          .setRestrictions(Restrictions.newBuilder()
-              // Restrict the API key usage by specifying the target service and methods.
-              // The API key can only be used to authenticate the specified methods in the service.
-              .addApiTargets(ApiTarget.newBuilder()
-                  .setService("translate.googleapis.com")
-                  .addMethods("translate.googleapis.com.TranslateText")
-                  .build())
-              .build())
-          .build();
 
-      // Initialize request and set arguments.
-      CreateKeyRequest createKeyRequest = CreateKeyRequest.newBuilder()
-          // API keys can only be global.
-          .setParent(LocationName.of(projectId, "global").toString())
-          .setKey(key)
-          .build();
+    File fileMetadata = new File();
+    fileMetadata.setName("photo.jpg");
+    // File's content.
+    java.io.File filePath = new java.io.File("files/photo.jpg");
+    // Specify media type and file-path for file.
+    FileContent mediaContent = new FileContent("image/jpeg", filePath);
 
-      // Make the request and wait for the operation to complete.
-      Key result = apiKeysClient.createKeyAsync(createKeyRequest).get(3, TimeUnit.MINUTES);
+    try { service.files().create(fileMetadata, mediaContent).setFields("id").execute(); } catch(IOException e) { System.out.println("Failed"); }
+    // try {
+    //   File file = service.files().create(fileMetadata, mediaContent)
+    //     .setFields("id")
+    //     .execute();
+    //   System.out.println("File ID: " + file.getId());
+    //   return file.getId();
+    // } catch (GoogleJsonResponseException e) {
+    //   // TODO(developer) - handle error appropriately
+    //   System.err.println("Unable to upload file: " + e.getDetails());
+    //   throw e;
+    // }
+    
+    // Print the names and IDs for up to 10 files.
+    FileList result = service.files().list()
+      .setPageSize(10)
+      .setFields("nextPageToken, files(id, name)")
+      .execute();
+    List<File> files = result.getFiles();
+    if (files == null || files.isEmpty()) {
+      System.out.println("No files found.");
+    } else {
+      System.out.println("Files:");
+      for (File file : files) {
+        System.out.printf("%s (%s)\n", file.getName(), file.getId());
+      }
 
-      // For authenticating with the API key, use the value in "result.getKeyString()".
-      // To restrict the usage of this API key, use the value in "result.getName()".
-      System.out.printf("Successfully created an API key: %s", result.getName());
+    }
+    
+  }
+
+
+
+
+  public static String uploadBasic() throws IOException {
+    // Load pre-authorized user credentials from the environment.
+    // TODO(developer) - See https://developers.google.com/identity for
+    // guides on implementing OAuth2 for your application.
+    GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+        .createScoped(Arrays.asList(DriveScopes.DRIVE_FILE));
+    HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
+        credentials);
+
+    // Build a new authorized API client service.
+    Drive service = new Drive.Builder(new NetHttpTransport(),
+        GsonFactory.getDefaultInstance(),
+        requestInitializer)
+        .setApplicationName("Drive samples")
+        .build();
+    // Upload file photo.jpg on drive.
+    File fileMetadata = new File();
+    fileMetadata.setName("photo.jpg");
+    // File's content.
+    java.io.File filePath = new java.io.File("files/photo.jpg");
+    // Specify media type and file-path for file.
+    FileContent mediaContent = new FileContent("image/jpeg", filePath);
+    try {
+      File file = service.files().create(fileMetadata, mediaContent)
+          .setFields("id")
+          .execute();
+      System.out.println("File ID: " + file.getId());
+      return file.getId();
+    } catch (GoogleJsonResponseException e) {
+      // TODO(developer) - handle error appropriately
+      System.err.println("Unable to upload file: " + e.getDetails());
+      throw e;
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import com.google.api.client.auth.oauth2.Credential;
-// import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-// import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-// import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-// import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-// import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-// import com.google.api.client.http.javanet.NetHttpTransport;
-// import com.google.api.client.json.JsonFactory;
-// import com.google.api.client.json.gson.GsonFactory;
-// import com.google.api.client.util.store.FileDataStoreFactory;
-// import com.google.api.services.drive.Drive;
-// import com.google.api.services.drive.DriveScopes;
-// import com.google.api.services.drive.model.File;
-// import com.google.api.services.drive.model.FileList;
-// import java.io.FileNotFoundException;
-// import java.io.IOException;
-// import java.io.InputStream;
-// import java.io.InputStreamReader;
-// import java.security.GeneralSecurityException;
-// import java.util.Collections;
-// import java.util.List;
-
-
-
-// /* class to demonstrate use of Drive files list API */
-// public class GoogleDrive {
-//   /**
-//    * Application name.
-//    */
-//   private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
-//   /**
-//    * Global instance of the JSON factory.
-//    */
-//   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-//   /**
-//    * Directory to store authorization tokens for this application.
-//    */
-//   private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-//   /**
-//    * Global instance of the scopes required by this quickstart.
-//    * If modifying these scopes, delete your previously saved tokens/ folder.
-//    */
-//   private static final List<String> SCOPES =
-//       Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
-//   private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-
-//   /**
-//    * Creates an authorized Credential object.
-//    *
-//    * @param HTTP_TRANSPORT The network HTTP Transport.
-//    * @return An authorized Credential object.
-//    * @throws IOException If the credentials.json file cannot be found.
-//    */
-//   private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-//       throws IOException {
-//     // Load client secrets.
-//     InputStream in = GoogleDrive.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-//     if (in == null) {
-//       throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-//     }
-//     GoogleClientSecrets clientSecrets =
-//         GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-//     // Build flow and trigger user authorization request.
-//     GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-//         HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-//         .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-//         .setAccessType("offline")
-//         .build();
-//     LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-//     Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-//     //returns an authorized Credential object.
-//     return credential;
-//   }
-
-//   public static void run(/*String... args*/) throws IOException, GeneralSecurityException {
-//     // Build a new authorized API client service.
-//     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-//     Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-//         .setApplicationName(APPLICATION_NAME)
-//         .build();
-
-//     // Print the names and IDs for up to 10 files.
-//     FileList result = service.files().list()
-//         .setPageSize(10)
-//         .setFields("nextPageToken, files(id, name)")
-//         .execute();
-//     List<File> files = result.getFiles();
-//     if (files == null || files.isEmpty()) {
-//       System.out.println("No files found.");
-//     } else {
-//       System.out.println("Files:");
-//       for (File file : files) {
-//         System.out.printf("%s (%s)\n", file.getName(), file.getId());
-//       }
-//     }
-//   }
-// }
